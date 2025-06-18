9603bc081a34a05a8b22c67277b00237092596e7
anythinks <alauddinazhary05@gmail.com>
2025-06-18 13:13:30 +0700

    fix: correct section title from "Ability" to "Skill" and standardize string quotes in AbilityView component

--- src/views/ability/AbilityView.jsx
+++ src/views/ability/AbilityView.jsx
@@ -1,27 +1,30 @@
-import './ability.css';
-import Image1 from '../../assets/ability-1.svg';
-import Image2 from '../../assets/ability-2.svg';
-import Image3 from '../../assets/ability-3.svg';
-import { Fade, Bounce } from 'react-awesome-reveal';
+import "./ability.css";
+import Image1 from "../../assets/ability-1.svg";
+import Image2 from "../../assets/ability-2.svg";
+import Image3 from "../../assets/ability-3.svg";
+import { Bounce } from "react-awesome-reveal";
 
 const data = [
   {
     id: 1,
     image: Image1,
-    title: 'Android Development',
-    description: 'Develop Android applications using Java or Kotlin. This includes designing user interfaces, implementing features, and ensuring app performance and security.',
+    title: "Android Development",
+    description:
+      "Develop Android applications using Java or Kotlin. This includes designing user interfaces, implementing features, and ensuring app performance and security.",
   },
   {
     id: 2,
     image: Image2,
-    title: 'Web Development',
-    description: 'Create and maintain websites which includes creating user interfaces (UI), developing features and functionality, and optimizing website performance.',
+    title: "Web Development",
+    description:
+      "Create and maintain websites which includes creating user interfaces (UI), developing features and functionality, and optimizing website performance.",
   },
   {
     id: 3,
     image: Image3,
-    title: 'Database Administrator',
-    description: 'Design, manage and maintain databases. Ensure security, availability and integrity of data in the database.',
+    title: "Database Administrator",
+    description:
+      "Design, manage and maintain databases. Ensure security, availability and integrity of data in the database.",
   },
 ];
 
@@ -29,7 +32,7 @@ const AbilityView = () => {
   return (
     <section id="ability" className="ability container section">
       <div>
-        <h2 className="section_title">Ability</h2>
+        <h2 className="section_title">Skill</h2>
       </div>
 
       <div className="ability_container grid">
