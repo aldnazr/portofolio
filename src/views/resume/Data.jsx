1b60b5e6f5a3e25e3e78cb4545d7cc3ccf701d05
anythinks <alauddinazhary05@gmail.com>
2025-06-19 15:11:29 +0700

    fix: standardize string quotes in Data component; adjust flex-wrap in work_filters for layout consistency

--- src/views/resume/Data.jsx
+++ src/views/resume/Data.jsx
@@ -1,25 +1,27 @@
 const Data = [
   {
     id: 1,
-    category: 'education',
-    icon: 'icon-graduation',
-    year: 'Aug 2024 - Feb 2025',
-    title: 'PPTA Dinamika University',
-    desc: 'Revamping the Academic Final Project Service Center Website as part of my undergraduate thesis project.Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.',
-  },{
+    category: "education",
+    icon: "icon-graduation",
+    year: "Aug 2024 - Feb 2025",
+    title: "PPTA Dinamika University",
+    desc: "Revamping the Academic Final Project Service Center Website as part of my undergraduate thesis project.",
+  },
+  {
     id: 2,
-    category: 'education',
-    icon: 'icon-graduation',
-    year: 'Feb 2024 - Jun 2024',
-    title: 'Bangkit Academy',
-    desc: 'Bangkit Academy is an exclusive technology talent academy program led by Google, GoTo, and Traveloka, and is one of the official programs under Kampus Merdeka (MSIB).',
-  },{
+    category: "education",
+    icon: "icon-graduation",
+    year: "Feb 2024 - Jun 2024",
+    title: "Bangkit Academy",
+    desc: "Bangkit Academy is an exclusive technology talent academy program led by Google, GoTo, and Traveloka, and is one of the official programs under Kampus Merdeka (MSIB).",
+  },
+  {
     id: 3,
-    category: 'education',
-    icon: 'icon-graduation',
-    year: 'Feb 2023 - Jun 2023',
-    title: 'CV Aikari Harvest',
-    desc: 'Designing the user interface for the company\'s internal inventory application.',
+    category: "education",
+    icon: "icon-graduation",
+    year: "Feb 2023 - Jun 2023",
+    title: "CV Aikari Harvest",
+    desc: "Designing the user interface for the company's internal inventory application.",
   },
 ];
 
