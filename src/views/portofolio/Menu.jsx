7cae747a7d1144b2838662afdba9bf1cbbcc587d
anythinks <alauddinazhary05@gmail.com>
2025-06-25 15:00:13 +0700

    Refactor code structure for improved readability and maintainability

--- src/views/portofolio/Menu.jsx
+++ src/views/portofolio/Menu.jsx
@@ -1,49 +1,49 @@
-import Work1 from "../../assets/asclepius.svg";
-import Work2 from "../../assets/story_app.svg";
-import Work3 from "../../assets/github_user.svg";
-import Work4 from "../../assets/blendit.svg";
-import Work5 from "../../assets/portofolio.png";
-import Work6 from "../../assets/kriptoin.svg";
+import Asclepius from "../../assets/asclepius.svg";
+import StoryApp from "../../assets/story_app.svg";
+import GithubUsers from "../../assets/github_users.svg";
+import Blendit from "../../assets/blendit.svg";
+import Portofolio from "../../assets/portofolio.png";
+import Kriptoin from "../../assets/kriptoin.svg";
 
 const Menu = [
   {
     id: 1,
-    image: Work1,
+    image: Asclepius,
     title: "Asclepius - Cancer Detection",
     category: "Mobile",
     link: "https://github.com/aldnazr/android-tensorflow",
   },
   {
     id: 2,
-    image: Work6,
+    image: Kriptoin,
     title: "Kriptoin",
     category: "Mobile",
     link: "https://github.com/aldnazr/kriptoin",
   },
   {
     id: 3,
-    image: Work4,
+    image: Blendit,
     title: "Blendit - Makeup Recommendation",
     category: "Mobile",
     link: "https://github.com/aldnazr/blendit-capstone-project",
   },
   {
     id: 4,
-    image: Work2,
+    image: StoryApp,
     title: "Story App",
     category: "Mobile",
     link: "https://github.com/aldnazr/android-story-app",
   },
   {
     id: 5,
-    image: Work3,
-    title: "Github User",
+    image: GithubUsers,
+    title: "Github Users",
     category: "Mobile",
     link: "https://github.com/aldnazr/android-aplikasi-github-user",
   },
   {
     id: 6,
-    image: Work5,
+    image: Portofolio,
     title: "Web Porfolio",
     category: "Web",
     link: "https://github.com/aldnazr/portofolio",
