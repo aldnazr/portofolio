7ec422eae9a6bab66cb9f56326251b458d9fc67f
anythinks <alauddinazhary05@gmail.com>
2025-06-30 17:12:04 +0700

    fix: update CV content for clarity and improve button styles in CSS

--- src/components/button/OpenLinkButtonComponent.jsx
+++ src/components/button/OpenLinkButtonComponent.jsx
@@ -1,8 +1,11 @@
-
 const OpenLinkButtonComponent = (props) => (
-    <a href={props.redirect} className={props.className} style={{ textDecoration: 'none', color: '#fff' }}>
-        {props.text}
-    </a>
+  <a
+    href={props.redirect}
+    className={props.className}
+    style={{ textDecoration: "none" }}
+  >
+    {props.text}
+  </a>
 );
 
 export default OpenLinkButtonComponent;
