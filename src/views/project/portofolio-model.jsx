563aebc759a1ec2f26e6ea075e9eb6509bf15302
anythinks <alauddinazhary05@gmail.com>
2026-03-24 06:42:40 +0700

    update depedency

--- src/views/project/portofolio-model.jsx
+++ src/views/project/portofolio-model.jsx
@@ -7,49 +7,48 @@ import StoryApp from "../../assets/story_app.svg";
 
 export const Menu = [
   {
-    id: 1,
+    image: Asclepius,
+    title: "Ahsan Xpress - Driver App",
+    category: "Mobile",
+    link: "#porto",
+  },
+  {
     image: Asclepius,
     title: "Asclepius - Cancer Detection",
     category: "Mobile",
     link: "https://github.com/aldnazr/android-tensorflow",
   },
   {
-    id: 2,
     image: Kriptoin,
     title: "Vault Chain - Track Cryptocurrency",
     category: "Mobile",
     link: "https://github.com/aldnazr/vault-chain",
   },
   {
-    id: 3,
     image: Blendit,
     title: "Blendit - Makeup Recommendation",
     category: "Mobile",
     link: "https://github.com/aldnazr/bangkit-capstone",
   },
   {
-    id: 4,
     image: StoryApp,
     title: "Story App",
     category: "Mobile",
     link: "https://github.com/aldnazr/android-story-app",
   },
   {
-    id: 5,
     image: GithubUsers,
     title: "Github Users",
     category: "Mobile",
     link: "https://github.com/aldnazr/android-aplikasi-github-user",
   },
   {
-    id: 6,
     image: Portofolio,
     title: "Web Porfolio",
     category: "Web",
     link: "https://github.com/aldnazr/portofolio",
   },
   {
-    id: 7,
     image:
       "https://raw.githubusercontent.com/aldnazr/ppta/refs/heads/main/preview/3.png",
     title: "PPTA - Layanan Akademik",
@@ -57,7 +56,6 @@ export const Menu = [
     link: "https://github.com/aldnazr/ppta",
   },
   {
-    id: 8,
     image:
       "https://raw.githubusercontent.com/aldnazr/netbeans-transaction/refs/heads/main/preview/2.png",
     title: "Cashier Transaction",
@@ -65,15 +63,6 @@ export const Menu = [
     link: "https://github.com/aldnazr/netbeans-transaction",
   },
   {
-    id: 9,
-    image:
-      "https://raw.githubusercontent.com/aldnazr/exchange-converter/refs/heads/main/preview/1.png",
-    title: "Exchange Converter",
-    category: "Web",
-    link: "https://github.com/aldnazr/exchange-converter",
-  },
-  {
-    id: 10,
     image:
       "https://raw.githubusercontent.com/aldnazr/phone-store/refs/heads/main/preview/1.png",
     title: "Phone Store",
@@ -81,11 +70,10 @@ export const Menu = [
     link: "https://github.com/aldnazr/phone-store",
   },
   {
-    id: 11,
     image:
-      "https://raw.githubusercontent.com/aldnazr/interpol/refs/heads/master/preview/1.png",
+      "https://raw.githubusercontent.com/aldnazr/interpol/refs/heads/master/public/1.png",
     title: "Interpol",
     category: "Web",
-    link: "https://github.com/aldnazr/interpol",
+    link: "https://interpol-theta.vercel.app/",
   },
 ];
